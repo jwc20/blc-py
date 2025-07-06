ecd4d193e3c7d5dc1a2656a146c08f9cffe3f1c8
jwc20 <choi.jae100@gmail.com>
2025-07-06 16:31:43 +0900

    add logic for calculating

--- blc/blc.py
+++ blc/blc.py
@@ -1,20 +1,46 @@
-from enum import Enum
-from abc import ABC, abstractmethod
-from plates import Plate, AvailablePlates
+from plates import Plates
 from barbell import Barbell
+from typing import List, Tuple
 
 
 class Blc:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, weight, plates: Plates, barbell: Barbell) -> None:
+        self._weight = weight
+        self.plates = plates
+        self.barbell = barbell
 
+    def calculate_plates(self):
+        target_weight = self._weight
+        plates_to_use = []
 
-        
+        weight_to_load = (
+            target_weight - self.barbell.weight
+        ) / 2  # weights that need to be loaded for one side
 
+        if self.plates.use_collar:
+            weight_to_load -= 2.5
+
+        if weight_to_load <= 0:  # or weight_to_load >= 275:
+            raise ValueError("Invalid weight")
+
+        for weight, quantity in self.plates:
+            while weight_to_load >= weight and quantity >= 2:
+                weight_to_load -= weight
+                plates_to_use.append(weight)
+                quantity -= 2
+
+        return plates_to_use
 
 
 if __name__ == "__main__":
-    twenty_five = Plates.TWENTY_FIVE.value
-    print(twenty_five)
+    # input = input("Enter the weight: ")
+    weight = 100
 
-    input = input("Enter the weight: ")
+
+    blc = Blc(weight, Plates(use_collar=False), Barbell(weight=20, type="men"))
+
+    print(blc.calculate_plates())
+
+    blc2 = Blc(weight, Plates(use_collar=True), Barbell(weight=20, type="men"))
+
+    print(blc2.calculate_plates(), " + collars")
