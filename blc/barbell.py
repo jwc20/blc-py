e4ba5373d2d634ad3911252b0a03157e07aad9d8
jwc20 <choi.jae100@gmail.com>
2025-07-06 15:25:47 +0900

    add check

--- blc/barbell.py
+++ blc/barbell.py
@@ -1,5 +1,15 @@
 class Barbell:
     def __init__(self, weight=20, type="men"):
-        self.weight = weight
+        if type == "women":
+            self.weight = 15
+        else:
+            self.weight = weight
         self.type = type
+
+
+if __name__ == "__main__":
+    barbell = Barbell() 
+    print(barbell.type, barbell.weight)
     
+    barbell_w = Barbell(type="women") 
+    print(barbell_w.type, barbell_w.weight)
