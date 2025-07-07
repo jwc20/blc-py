15981c0a46a5d5843e17582a31da30c26a4d7687
jwc20 <choi.jae100@gmail.com>
2025-07-07 12:57:57 +0900

    Develop (#2)

--- tests/test_blc.py
+++ tests/test_blc.py
@@ -11,9 +11,8 @@ def test_blc_initialization():
     """Test that Blc initializes with correct attributes."""
     plates = Plates(use_collar=True)
     barbell = Barbell(weight=20, type="men")
-    blc = Blc(weight=100, plates=plates, barbell=barbell)
+    blc = Blc(plates=plates, barbell=barbell)
 
-    assert blc.weight == 100
     assert blc.plates == plates
     assert blc.barbell == barbell
     assert blc.plates_to_use == []
@@ -27,12 +26,12 @@ def test_calculate_plates_success():
     plates.add_plate(10, 4)  # 4 plates of 10kg
 
     barbell = Barbell(weight=20, type="men")
-    blc = Blc(weight=100, plates=plates, barbell=barbell)
+    blc = Blc(plates=plates, barbell=barbell)
 
     # 100kg total = 20kg bar + 80kg plates (40kg per side)
     # With collar (2.5kg each side), we need 42.5kg per side
     # Should use: 2x20kg + 2x1.25kg (but since we don't have 1.25kg, it will use what's available)
-    result = blc.calculate_plates()
+    result = blc.calculate_plates(weight=100)
 
     # The exact result depends on the implementation, but we can check some basics
     assert len(result) > 0
@@ -43,7 +42,7 @@ def test_remove_weight_not_available():
     """Test removing weight that's not on the barbell."""
     plates = Plates(use_collar=True)
     barbell = Barbell(weight=20, type="men")
-    blc = Blc(weight=20, plates=plates, barbell=barbell)
+    blc = Blc(plates=plates, barbell=barbell)
 
     with pytest.raises(ValueError):
         blc.remove_weight(10)  # Try to remove 10kg that's not there
